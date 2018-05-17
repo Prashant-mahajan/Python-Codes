0b3acc3726587d2fdaa4bfabf6dc5540cf4935e8
Prashant-mahajan <prashantmahajan70@gmail.com>
2018-05-17 21:41:14 +0530

    Getting hands on Linked List in Python

---
+++ Hackerrank/Data Structure/linkedListBasics.py
@@ -0,0 +1,20 @@
+"""
+ Print elements of a linked list on console
+ head input could be None as well for empty list
+ Node is defined as
+ 
+ class Node(object):
+ 
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+ 
+ 
+"""
+
+# Print elements of Linked List
+def print_list(head):
+    while head: 
+        print(head.data)
+        head = head.next
+        
