a85c344a3eb27f4b10a65a7b440d818115df8f28
Prashant-mahajan <prashantmahajan70@gmail.com>
2018-05-18 11:31:52 +0530

    Insertion and deletion in linked list

--- Hackerrank/Data Structure/linkedListBasics.py
+++ Hackerrank/Data Structure/linkedListBasics.py
@@ -29,3 +29,35 @@ def Insert(head, data):
         Insert(head.next, data)
     return head
     
+# Inserting new node at the start of linked list
+
+def Insert(head, data):
+    NewNode = Node(data)
+    NewNode.next = head
+    head = NewNode
+    return head
+
+# Insert node at a particular position in a linked list 
+
+def InsertNth(head, data, position):
+    if position==0:
+        return Node(data, head)
+    top = head
+    for _ in range(position-1):
+        top = top.next
+    NewNode = Node(data, top.next)
+    top.next = NewNode
+    return head
+
+# Deleting a node in a linked list 
+def Delete(head, position):
+    if position == 0:
+        head = head.next
+        return head
+    
+    top = head
+    for _ in range(position-1):
+        top = top.next
+    top.next = top.next.next
+
+    return head
