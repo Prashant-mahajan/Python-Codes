761ddd3841b60fafba424a3d39d970fa144d8711
Prashant-mahajan <prashantmahajan70@gmail.com>
2018-05-22 22:27:46 +0530

    Reverse of a linked list

---
+++ Hackerrank/Data Structure/Reverse a linked list.py
@@ -0,0 +1,20 @@
+"""
+ Reverse a linked list
+ head could be None as well for empty list
+ Node is defined as
+ 
+ class Node(object):
+ 
+   def __init__(self, data=None, next_node=None):
+       self.data = data
+       self.next = next_node
+
+ return back the head of the linked list in the below method.
+"""
+
+def Reverse(head):
+    prev = None
+    while head:
+        prev = Node(head.data, prev)
+        head = head.next
+    return prev
