46aa2f38e83195c0347b6205e9d7662424e3721a
Prashant-mahajan <prashantmahajan70@gmail.com>
2018-05-25 23:55:22 +0530

    Get Node value

---
+++ Hackerrank/Data Structure/getNodeValue.py
@@ -0,0 +1,62 @@
+# problem: https://www.hackerrank.com/challenges/get-the-value-of-the-node-at-a-specific-position-from-the-tail/problem
+
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+# Complete the getNode function below.
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+
+def GetNode(head, position):
+    tracked = head
+    while position > 0:
+        head = head.next
+        position -= 1
+        
+    while head.next:
+        head = head.next
+        tracked = tracked.next
+        
+    
