920cf1b5a8bcc3d49e3d79e531d457977a920023
Prashant-mahajan <prashantmahajan70@gmail.com>
2018-05-27 19:13:51 +0530

    Tree traversal methods

---
+++ Hackerrank/Data Structure/treeTraversal.py
@@ -0,0 +1,32 @@
+# Problem: https://www.hackerrank.com/challenges/tree-preorder-traversal/problem
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.data (the value of the node)
+"""
+def preOrder(root):
+    print(root.data) , # Prints values on the same line 
+    if root.left is not None:
+        preOrder(root.left)
+    if root.right is not None: 
+        preOrder(root.right)
+
+# Left, Right, Node
+def postOrder(root):
+    #Write your code here
+    if root.left is not None:
+        postOrder(root.left)
+    if root.right is not None:
+        postOrder(root.right)
+    print(root.data) , 
+
+# Left, Node, Right
+def inOrder(root):
+    #Write your code here
+    if root.left is not None:
+        inOrder(root.left)
+    print(root.data) ,
+    if root.right is not None:
+        inOrder(root.right)
