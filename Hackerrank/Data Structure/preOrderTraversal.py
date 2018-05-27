2db6cc270798e5f0e10ce2ab0b80d5f44cc9cf8f
Prashant-mahajan <prashantmahajan70@gmail.com>
2018-05-27 18:53:14 +0530

    Trees:Pre-order Traversal

---
+++ Hackerrank/Data Structure/preOrderTraversal.py
@@ -0,0 +1,15 @@
+# Problem: https://www.hackerrank.com/challenges/tree-preorder-traversal/problem
+
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.data (the value of the node)
+"""
+def preOrder(root):
+    print(root.data) ,
+    if root.left is not None:
+        preOrder(root.left)
+    if root.right is not None: 
+        preOrder(root.right)
+    
