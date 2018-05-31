08f12df5610f9b64582cfb8bd5b820215ea24cbe
Prashant-mahajan <prashantmahajan70@gmail.com>
2018-06-01 01:27:05 +0530

    Maximum Element of Stack

---
+++ Hackerrank/Data Structure/maxElementOfStack.py
@@ -0,0 +1,28 @@
+class Stack:
+    def __init__(self):
+        self.items = [0]
+        self.maximum = [0]
+
+    def isEmpty(self):
+       return self.items == []
+
+    def push(self, item):
+        self.items.append(item)
+        if self.maximum[-1] <= item:
+            self.maximum.append(item)
+            
+    def pop(self):
+        if self.items[-1] == self.maximum[-1]:
+            self.maximum.pop()
+        self.items.pop()
+
+
+s = Stack()
+for _ in range(int(input())):
+    num = input().split()
+    if num[0] == '1': 
+        s.push(int(num[1]))
+    elif num[0] == '2':
+        s.pop()
+    else: 
+        print(s.maximum[-1])
