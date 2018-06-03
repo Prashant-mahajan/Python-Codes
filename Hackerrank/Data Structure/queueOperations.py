542699667858e8a3e3b414a1ed392be882f6b9ba
Prashant-mahajan <prashantmahajan70@gmail.com>
2018-06-03 23:51:29 +0530

    Queue using two stacks

---
+++ Hackerrank/Data Structure/queueOperations.py
@@ -0,0 +1,14 @@
+# Problem: https://www.hackerrank.com/challenges/queue-using-two-stacks/problem
+
+from collections import deque
+
+queue = deque([])
+
+for _ in range(int(input())):
+    num = input().split()
+    if num[0] == '1': 
+        queue.append(int(num[1]))
+    elif num[0] == '2':
+        queue.popleft()
+    else: 
+        print(queue[0])
