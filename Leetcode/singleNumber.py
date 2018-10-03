c117b9f8a3a592f343ede14545da07ee9842d812
Prashant-mahajan <prashantmahajan70@gmail.com>
2018-10-03 17:56:05 -0400

    concept of XOR to find a hidden number

---
+++ Leetcode/singleNumber.py
@@ -0,0 +1,19 @@
+class Solution:
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+
+        # Using Dictionaries
+        num_dict = dict([i, nums.count(i)] for i in nums)
+        for key, value in num_dict.items():
+            if value == 1:
+                return key
+        
+        # Using concept of XOR 
+        number = 0
+        for i in nums:
+            number = number ^ i 
+        return number 
+            
