7a0b1118e058fa9a05da958fdc784e5ffa78bdb8
Prashant-mahajan <prashantmahajan70@gmail.com>
2018-10-16 19:35:58 -0400

    Min element from stack

---
+++ Leetcode/155minStack.py
@@ -0,0 +1,37 @@
+class MinStack:
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        self.stack.append(x)
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        if self.stack:
+            return self.stack.pop()
+        else:
+            return False 
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        if self.stack:
+            return min(self.stack)
+
