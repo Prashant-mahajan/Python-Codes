479aa2a47adc9246d0bd65c07c108d9076e142ed
Prashant-mahajan <prashantmahajan70@gmail.com>
2019-01-20 22:14:43 -0500

    Script to download large csv files from given link

--- Scripts/scrapper.py
+++ Scripts/scrapper.py
@@ -1,7 +1,7 @@
 import requests
 import shutil
 
-url = "https://storage.googleapis.com/openimages/2018_04/test/test-images-with-rotation.csv"
+url = "insert url here"
 
 local_filename = url.split('/')[-1]
 r = requests.get(url, stream=True)
